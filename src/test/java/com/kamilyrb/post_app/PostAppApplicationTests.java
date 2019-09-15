7372219445547ef08ce0657872dcea702034e03d
kamilyrb <kamilyrb@gmail.com>
2019-09-15 19:03:34 +0300

    Initial commit

---
+++ src/test/java/com/kamilyrb/post_app/PostAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kamilyrb.post_app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
